1d588c8e2225994bf5eab021e523b572e73b3107
Raphaell <rafaellkf@gmail.com>
2024-04-18 09:32:46 -0300

    Add novos Pattern

--- dist/enums/pattern.js
+++ dist/enums/pattern.js
@@ -5,5 +5,11 @@ var Pattern;
 (function (Pattern) {
     Pattern["QTD_DISPOSITIVOS_CONECTADOS"] = "QTD_DISPOSITIVOS_CONECTADOS";
     Pattern["CONEXAO_FECHADA"] = "CONEXAO_FECHADA";
+    Pattern["DESCONHECIDA"] = "DESCONHECIDA";
+    Pattern["NOTIFICACAO"] = "NOTIFICACAO";
+    Pattern["LOCALIZACAO"] = "LOCALIZACAO";
+    Pattern["HEARTBEAT"] = "HEARTBEAT";
+    Pattern["COMANDO"] = "COMANDO";
+    Pattern["LOGIN"] = "LOGIN";
 })(Pattern || (exports.Pattern = Pattern = {}));
 //# sourceMappingURL=pattern.js.map
